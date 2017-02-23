73cfeff0e565bcb20d23360a37d38138ccbb8d69
p15215061 <p15215061@GWHXX682WIN016A>
2017-02-23 16:56:40 +0000

    jnkn

--- TestFramework(Aneeka)/tstCustomerDetails.cs
+++ TestFramework(Aneeka)/tstCustomerDetails.cs
@@ -3,7 +3,7 @@ using System.Text;
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ClassLibrary;
-
+//lolpollpol
 namespace MyTestFrame
 {
     /// <summary>
