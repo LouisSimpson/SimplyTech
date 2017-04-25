6449fadbadd455c3a6033eefca6ab5ceaa013bef
p15231608 <p15231608@GWHXX682WIN020W>
2017-04-25 16:04:10 +0100

    changes made to stored procedure, and order collection code for count properties

--- ClassLibrary/clsOrderCollection.cs
+++ ClassLibrary/clsOrderCollection.cs
@@ -8,25 +8,30 @@ namespace ClassLibrary
     {
         public clsOrderCollection()
         {
-            clsOrder TestItem = new clsOrder();
-            //set its properties
-            TestItem.OrderNo = 1;
-            TestItem.DateOrdered = DateTime.Now.Date;
-            TestItem.ProductName = "some name";
-            TestItem.QuantityNo = 1;
-            TestItem.OrderPrice = 100;
-            //add order to test list
-            mOrderList.Add(TestItem);
-            //reinitialise the object fo rsome new data
-            TestItem = new clsOrder();
-            //set properties
-            TestItem.OrderNo = 2;
-            TestItem.DateOrdered = DateTime.Now.Date;
-            TestItem.ProductName = "some name";
-            TestItem.QuantityNo = 2;
-            TestItem.OrderPrice = 200;
-            //add item to test list
-            mOrderList.Add(TestItem);
+            //var for the index
+            Int32 Index = 0;
+            //var to store record count
+            Int32 RecordCount = 0;
+            //object for data connection
+            clsDataConnection DB = new clsDataConnection();
+            //Execute the stored procedure
+            DB.Execute("sproc_tblOrder_SelectAll");
+            //get the count of records to process
+            while (Index < RecordCount)
+            {
+                //create blank order
+                clsOrder AnOrder = new clsOrder();
+                //read in fields from the current records in the tbale
+                AnOrder.OrderNo = Convert.ToInt32(DB.DataTable.Rows[Index]["OrderNo"]);
+                AnOrder.DateOrdered = Convert.ToDateTime(DB.DataTable.Rows[Index]["DateOrdered"]);
+                AnOrder.ProductName = Convert.ToString(DB.DataTable.Rows[Index]["ProductName"]);
+                AnOrder.QuantityNo = Convert.ToInt32(DB.DataTable.Rows[Index]["Quantity"]);
+                AnOrder.OrderPrice = Convert.ToDecimal(DB.DataTable.Rows[Index]["OrderPrice"]);
+                //add record to private data member
+                mOrderList.Add(AnOrder);
+                //point at next record
+                Index++;
+            }
 
         }
         //private data member for the list
